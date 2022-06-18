bcf16b4470066af2645343c1eff1907a43ef05da
Aniket Gavali <80736503+aniket-dg@users.noreply.github.com>
2022-06-18 10:38:53 +0530

    Time: 56 ms (37.03%), Space: 14.3 MB (48.21%) - LeetHub

--- 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.py
+++ 107-binary-tree-level-order-traversal-ii/107-binary-tree-level-order-traversal-ii.py
@@ -6,23 +6,28 @@
 #         self.right = right
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
-        from queue import Queue
-        if not root:
-            return []
-        q = Queue()
-        q.put(root)
-        res = []
-        while not q.empty():
-            temp = Queue()
-            temp_res = []
-            while not q.empty():
-                curr = q.get()
-                temp_res.append(curr.val)
-                if curr.left:
-                    temp.put(curr.left)
-                if curr.right:
-                    temp.put(curr.right)
+        queue, res = collections.deque([root, 0]), []
+        while queue:
+            res.append([node.val for node in queue if node])
+            queue = [child for node in queue if node for child in (node.left, node.right) if node]
+        return res[-2::-1]
+#         from queue import Queue
+#         if not root:
+#             return []
+#         q = Queue()
+#         q.put(root)
+#         res = []
+#         while not q.empty():
+#             temp = Queue()
+#             temp_res = []
+#             while not q.empty():
+#                 curr = q.get()
+#                 temp_res.append(curr.val)
+#                 if curr.left:
+#                     temp.put(curr.left)
+#                 if curr.right:
+#                     temp.put(curr.right)
             
-            q = temp
-            res.insert(0, temp_res)
-        return res
+#             q = temp
+#             res.insert(0, temp_res)
+#         return res
