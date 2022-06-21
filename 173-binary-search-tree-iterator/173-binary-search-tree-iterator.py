4cd46b77756cb0161da389d134aab9300c759222
Aniket Gavali <80736503+aniket-dg@users.noreply.github.com>
2022-06-21 17:42:36 +0530

    Time: 95 ms (63.51%), Space: 20.4 MB (64.81%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.py
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.py
@@ -6,14 +6,35 @@
 #         self.right = right
 class BSTIterator:
 
+#     def __init__(self, root: Optional[TreeNode]):
+#         self.stack = self.inorder(root)[::-1]
+
+#     def next(self) -> int:
+#         return self.stack.pop()
+
+#     def hasNext(self) -> bool:
+#         return len(self.stack)>0
+        
+#     def inorder(self, root):
+#         if not root:
+#             return []
+#         return self.inorder(root.left) + [root.val] + self.inorder(root.right)
+
     def __init__(self, root: Optional[TreeNode]):
-        self.stack = self.inorder(root)[::-1]
+        self.current_node = root
+        self.root = root
+        self.stack = []
 
     def next(self) -> int:
-        return self.stack.pop()
+        while self.current_node:
+            self.stack.append(self.current_node)
+            self.current_node = self.current_node.left
+        next=  self.stack.pop()
+        self.current_node=  next.right
+        return next.val
 
     def hasNext(self) -> bool:
-        return len(self.stack)>0
+        return self.current_node is not None or len(self.stack)>0
         
     def inorder(self, root):
         if not root:
