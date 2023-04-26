cf3f82dfde70e19a56422aed0ce8573047fec4f3
Aniket Gavali <80736503+aniket-dg@users.noreply.github.com>
2023-04-26 13:39:43 +0530

    Time: 48 ms (24.57%) | Memory: 14.3 MB (13.96%) - LeetSync

---
+++ add-strings/add-strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        result = []
+        carry = 0
+        i, j = len(num1)-1, len(num2)-1
+        while i >= 0 or j >= 0 or carry:
+            n1 = int(num1[i]) if i >= 0 else 0
+            n2 = int(num2[j]) if j >= 0 else 0
+
+            carry, mod = divmod(n1 + n2 + carry, 10)
+            result.append(str(mod))
+            i, j = i-1, j-1
+        
+        return "".join(result)[::-1]
