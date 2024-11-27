921349b54b3935bf5f65187692e44d4aef57c6db
Aniket Gavali <80736503+aniket-dg@users.noreply.github.com>
2024-11-27 15:21:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (10.48%) - LeetSync

---
+++ string/1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -0,0 +1,8 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort()
+        for i in range(len(nums)):
+            x = len(nums) - i
+            if nums[i]>= x and (i == 0 or nums[i-1] < x):
+                return x
+        return -1
