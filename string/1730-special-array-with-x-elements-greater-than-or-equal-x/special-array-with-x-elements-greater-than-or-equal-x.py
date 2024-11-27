e3b285dd53f9547d4b6e5e121c7982448d62b124
Aniket Gavali <80736503+aniket-dg@users.noreply.github.com>
2024-11-27 15:26:03 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (10.48%) - LeetSync

--- string/1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ string/1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,8 +1,15 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         nums.sort()
-        for i in range(len(nums)):
-            x = len(nums) - i
-            if nums[i]>= x and (i == 0 or nums[i-1] < x):
-                return x
+        l, r = 0, len(nums)
+        while l <= r:
+            mid = l + (r-l+1)//2
+            greater_than_mid = len(nums) - bisect_left(nums, mid)
+            print(mid, greater_than_mid)
+            if greater_than_mid == mid:
+                return mid
+            elif greater_than_mid < mid:
+                r = mid - 1
+            else:
+                l = mid + 1
         return -1
