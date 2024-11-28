d6a2fb2c4e1f08a0076624f1f48f708510c1a889
Aniket Gavali <80736503+aniket-dg@users.noreply.github.com>
2024-11-28 08:21:10 +0530

    Time: 11 ms (50.58%) | Memory: 35.6 MB (30.47%) - LeetSync

--- string/2634-minimum-common-value/minimum-common-value.py
+++ string/2634-minimum-common-value/minimum-common-value.py
@@ -1,21 +1,30 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        def search(nums, k):
-            l, r = 0, len(nums)-1
-            while l <= r:
-                mid = l + (r-l+1)//2
-                if nums[mid]==k:
-                    return True
-                elif nums[mid] < k: 
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            return False
-        
-        if len(nums2) < len(nums1):
-            nums1, nums2 = nums2, nums1 
-
-        for item in nums1:
-            if search(nums2, item):
-                return item
+        i = j = 0
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                return nums1[i]
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
         return -1
+        # def search(nums, k):
+        #     l, r = 0, len(nums)-1
+        #     while l <= r:
+        #         mid = l + (r-l+1)//2
+        #         if nums[mid]==k:
+        #             return True
+        #         elif nums[mid] < k: 
+        #             l = mid + 1
+        #         else:
+        #             r = mid - 1
+        #     return False
+        
+        # if len(nums2) < len(nums1):
+        #     nums1, nums2 = nums2, nums1 
+
+        # for item in nums1:
+        #     if search(nums2, item):
+        #         return item
+        # return -1
